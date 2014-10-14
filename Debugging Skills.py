074de92277bfd164e030d9f77a97a5bad4379fdd
JacksonMcGlothlin <jwmcglothlin@me.com>
2014-10-14 13:35:50 -0500

    First Commit

---
+++ Debugging Skills.py
@@ -0,0 +1,24 @@
+total = 0
+print 'How many numbers would you like to add together?'
+userInput = int(raw_input())
+for x in range(userInput):
+    total = total + userInput
+print total
+
+
+
+totalCost = []
+for W in range(userInput):
+    print 'How much did the item cost?'
+    userInput = int(raw_input())
+    totalCost.append(userInput)
+print sum(totalCost)
+
+
+
+total = []
+print 'what number do you want to take the factorial of?'
+userInput = int(raw_input())
+for x in range():
+    total = total * x
+print total
